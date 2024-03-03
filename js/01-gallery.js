f6c7c8d1d14dcc40167a9086cd9feca3460f6e85
NastiHluhan <0506167197more@gmail.com>
2024-03-03 12:36:27 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,2 +1,21 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
